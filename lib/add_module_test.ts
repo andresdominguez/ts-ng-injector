9d6fb09c45d8d52fdbbd37460a2d8aff9e4e05ad
andresdom <andesdom>
2017-12-12 23:06:09 -0500

    rename module

--- lib/add_module_test.ts
+++ lib/add_module_test.ts
@@ -4,8 +4,8 @@ import {join} from 'path';
 test('adds 1 + 2 to equal 3', () => {
   let path = join('test/before_inject.ts');
   let sourceFile = parseFile(path);
-  let file = addModule(sourceFile, 'CholoModule', 'foo/bar');
-  expect(printFile(file)).toBe(`import { CholoModule } from "foo/bar";
+  let file = addModule(sourceFile, 'SomeModule', 'foo/bar');
+  expect(printFile(file)).toBe(`import { SomeModule } from "foo/bar";
 import { NgModule } from '@angular/core';
 @NgModule({
     imports: []
