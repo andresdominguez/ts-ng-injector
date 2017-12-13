b2338f50795b0b56a97dc59cd05e0c6c1c43a371
andresdom <andesdom>
2017-12-12 23:23:14 -0500

    add import to NgModule

--- lib/add_module.ts
+++ lib/add_module.ts
@@ -5,10 +5,49 @@ export function parseFile(fileName: string): ts.SourceFile {
   return ts.createSourceFile(fileName, readFileSync(fileName).toString(), ts.ScriptTarget.ES2015, /*setParentNodes */ true);
 }
 
-export function addModule(sourceFile: ts.SourceFile,
+export function addImport(sourceFile: ts.SourceFile,
                           moduleName: string, importPath: string): ts.SourceFile {
   const importDeclaration = createImportDeclaration(moduleName, importPath);
   return ts.updateSourceFileNode(sourceFile, [importDeclaration, ...sourceFile.statements]);
+}
+
+function hasNgModuleDecorator(classDeclaration: ts.ClassDeclaration): boolean {
+  if (classDeclaration.decorators && classDeclaration.decorators.length) {
+    const decorator = classDeclaration.decorators[0] as ts.Decorator;
+    const callExpression = decorator.expression as ts.CallExpression;
+    return callExpression.expression.getText() === 'NgModule';
+  }
+  return false;
+}
+
+export function addToNgModuleImports(sourceFile: ts.SourceFile, moduleName: string): ts.SourceFile {
+  visitNodes(sourceFile);
+
+  function visitNodes(node: ts.Node) {
+    if (node.kind === ts.SyntaxKind.ClassDeclaration) {
+      const classDeclaration = node as ts.ClassDeclaration;
+      if (hasNgModuleDecorator(classDeclaration)) {
+        const decorator = classDeclaration.decorators[0];
+        const callExpression = decorator.expression as ts.CallExpression;
+        const objectLiteralExpression = callExpression.arguments[0] as ts.ObjectLiteralExpression;
+        const properties = objectLiteralExpression.properties;
+
+        const found = properties.filter(p => p.name.getText() === 'imports');
+        if (found.length > 0) {
+          const importsProp = found[0] as ts.PropertyAssignment;
+          const initializer = importsProp.initializer as ts.ArrayLiteralExpression;
+
+          const copy = [...initializer.elements.slice(0), ts.createIdentifier(moduleName)];
+
+          importsProp.initializer = ts.updateArrayLiteral(initializer, ts.createNodeArray(copy, true));
+        }
+      }
+    }
+
+    ts.forEachChild(node, visitNodes);
+  }
+
+  return sourceFile;
 }
 
 function createImportDeclaration(name: string, pathTo: string): ts.ImportDeclaration {
--- lib/add_module_test.ts
+++ lib/add_module_test.ts
@@ -1,14 +1,27 @@
-import {addModule, parseFile, printFile} from "./add_module";
+import {addImport, addToNgModuleImports, parseFile, printFile} from "./add_module";
 import {join} from 'path';
 
-test('adds 1 + 2 to equal 3', () => {
+test('Adds ts import', () => {
   let path = join('test/before_inject.ts');
   let sourceFile = parseFile(path);
-  let file = addModule(sourceFile, 'SomeModule', 'foo/bar');
+  let file = addImport(sourceFile, 'SomeModule', 'foo/bar');
   expect(printFile(file)).toBe(`import { SomeModule } from "foo/bar";
 import { NgModule } from '@angular/core';
 @NgModule({
     imports: []
+})
+export class TestModule {
+}
+`);
+});
+
+test('Add NgModule import', () => {
+  let path = join('test/before_inject.ts');
+  let sourceFile = parseFile(path);
+  let file = addToNgModuleImports(sourceFile, 'SomeModule');
+  expect(printFile(file)).toBe(`import { NgModule } from '@angular/core';
+@NgModule({
+    imports: [SomeModule,]
 })
 export class TestModule {
 }
