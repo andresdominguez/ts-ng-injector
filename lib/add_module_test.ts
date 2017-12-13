f6dc4a7524f9e021b5b89048bd6f74d8dcf0d7c2
andresdom <andesdom>
2017-12-12 23:05:22 -0500

    Add proper test to add import

--- lib/add_module_test.ts
+++ lib/add_module_test.ts
@@ -5,5 +5,12 @@ test('adds 1 + 2 to equal 3', () => {
   let path = join('test/before_inject.ts');
   let sourceFile = parseFile(path);
   let file = addModule(sourceFile, 'CholoModule', 'foo/bar');
-  console.log(printFile(file));
+  expect(printFile(file)).toBe(`import { CholoModule } from "foo/bar";
+import { NgModule } from '@angular/core';
+@NgModule({
+    imports: []
+})
+export class TestModule {
+}
+`);
 });
