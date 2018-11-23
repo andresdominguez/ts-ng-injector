902bd607f886a7014f940622b674973041ef5bac
andresdom <andesdom>
2018-11-22 21:35:45 -0500

    add comments, simplify

--- lib/find_imports.ts
+++ lib/find_imports.ts
@@ -7,25 +7,25 @@ export interface ImportInfo {
   from: string;
 }
 
+/**
+ * Finds all the "import {foo} from 'path'".
+ */
 export function findImports(sourceFile: ts.SourceFile): Maybe<ImportInfo[]> {
   return Maybe.lift(sourceFile)
       .fmap(filterByKind<ts.ImportDeclaration>(ts.SyntaxKind.ImportDeclaration))
-      .fmap((imports: ts.ImportDeclaration[]) => {
+      .fmap(imports => {
         return imports.map((importDeclaration: ts.ImportDeclaration) => {
+          // Drop quotes from the import.
           const from = importDeclaration.moduleSpecifier.getText().replace(/['"]/g, '');
+
+          // Collect all the identifiers.
           return Maybe.lift(importDeclaration.importClause)
               .fmap(filterByKind<ts.Identifier>(ts.SyntaxKind.Identifier))
               .fmap(identifiers => identifiers.map(getText))
-              .fmap(ids => {
-                return ids.map(id => {
-                  return {
-                    identifier: id,
-                    from: from,
-                  };
-                });
-              })
+              .fmap(ids => ids.map(identifier => ({identifier, from})))
               .unwrap();
         });
       })
+      // Flatten.
       .fmap(x => [].concat.apply([], x));
 }
